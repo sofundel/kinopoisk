456bfedcfb98d756486de7abc84b7bb87135a482
sofundel <sofundel@yandex.ru>
2023-08-17 20:23:03 +0300

    kinopoisk app for GitHub account

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    "airbnb",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react-refresh/only-export-components": "warn",
+    "no-console": ["error", { allow: ["warn", "error"] }],
+  },
+};
